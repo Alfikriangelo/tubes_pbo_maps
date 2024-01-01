136e4991e7789795598fdd86154644f85c2576eb
Darell <darell.adyatma21@gmail.com>
2024-01-01 17:25:08 +0700

    Done Login + Session

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import React from 'react'
-import Maps from './Pages/Maps'
-import Login from './Pages/Login'
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthGuard from './authGuard';
+import Login from './Pages/Login';
+import Maps from './Pages/Maps';
+import { AuthProvider } from './authContext'; // Import AuthProvider dari AuthContext.js
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Login />}/>
-        <Route path='/maps' element={<Maps />}/>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/maps" element={<AuthGuard element={<Maps />} />} />
+        </Routes>
+      </AuthProvider>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,47 +1,88 @@
-import React, { useState } from 'react'
+// Login.js
+import React, { useState } from 'react';
+import {useNavigate } from 'react-router-dom';
+import { useAuth } from '../authContext';
 import Button from '@mui/material/Button';
-import './Login.css'
+import './Login.css';
 
-import user_icon from '../Components/Assets/person.png'
-import email_icon from '../Components/Assets/email.png'
-import password_icon from '../Components/Assets/password.png'
+import user_icon from '../Components/Assets/person.png';
+import password_icon from '../Components/Assets/password.png';
 
 const Login = () => {
+  const { login } = useAuth();
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-  const [action, setAction] = useState("Sign Up");
+  const handleLogin = async () => {
+    const user = { name: 'admin', password: 'admin' };
+
+    // Bandingkan data dengan input pengguna
+    if (user && user.name === name && user.password === password) {
+      console.log('Login berhasil', user);
+      setError(null);
+      login();
+      navigate('/maps');
+    } else {
+      console.error('Login gagal');
+      setError('Login gagal. Periksa kembali nama pengguna dan kata sandi.');
+    }
+  };
 
   return (
     <div className='container'>
       <div className='header'>
-        <div className='text'>
-          {action}
-        </div>
+        <div className='text'>Login</div>
         <div className='underline'></div>
       </div>
       <div className='inputs'>
-        {action === 'Login' ? null : (
-          <div className="input">
-            <img src={user_icon} alt="" />
-            <input type="text" placeholder='Name' />
-          </div>
-        )}
-        <div className="input">
-          <img src={email_icon} alt="" />
-          <input type="email" placeholder='Email' />
+        <div className='input'>
+          <img src={user_icon} alt='' />
+          <input
+            type='text'
+            placeholder='Name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-        <div className="input">
-          <img src={password_icon} alt="" />
-          <input type="password" placeholder='Password' />
+        <div className='input'>
+          <img src={password_icon} alt='' />
+          <input
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
       </div>
-      {action==='Sign Up'? null :<div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
-      <div className="submit-container">
-        <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sign Up") }}>Sign Up</div>
-        <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+      <div className='forgot-password'>
+        Lost Password? <span>Click Here!</span>
       </div>
-      <Button variant="contained" href='/maps'>Maps</Button>
+      <div className='submit-container'>
+        <Button
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '220px',
+            height: '59px',
+            color: '#fff',
+            background: '#4c00b4',
+            borderRadius: '50px',
+            fontSize: '19px',
+            fontWeight: '700',
+            cursor: 'pointer',
+          }}
+          variant='contained'
+          onClick={handleLogin}
+        >
+          Login
+        </Button>
+      </div>
+      {error && <div className='error-message'>{error}</div>}
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
